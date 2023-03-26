6c3ec56ad48680f30d36ac73fb1a4188e7fcc901
Celia Ji <celiaji@google.com>
2023-03-26 18:53:25 +0000

    adding gcs file path reader

--- tools/bigquery-antipattern-tool/src/main/java/com/pso/bigquery/optimization/antipatterns/cmd/InputQueryIterable.java
+++ tools/bigquery-antipattern-tool/src/main/java/com/pso/bigquery/optimization/antipatterns/cmd/InputQueryIterable.java
@@ -5,6 +5,11 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Iterator;
 import java.util.List;
+import com.google.cloud.storage.Bucket;
+import com.google.cloud.storage.BucketInfo;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+
 
 public class InputQueryIterable implements Iterator<InputQuery> {
 
@@ -24,7 +29,22 @@ public class InputQueryIterable implements Iterator<InputQuery> {
     String filePathStr = filePathIterator.next();
     Path fileName = Path.of(filePathStr);
     try {
+      if (filePathStr.startsWith("gs://")){
+      System.out.println(fileName);
+//      example: gs://anti_pattern_sql/query_file1.sql
+      String bucketName = "anti_pattern_sql";
+      String blobName = "query_file1.sql";
+      Storage storage = StorageOptions.getDefaultInstance().getService();
+      BlobId blobId = BlobId.of(bucketName, blobName);
+      Blob blob = storage.get(blobId);
+      long size = blob.getSize(); // no RPC call is required
+      byte[] content = blob.getContent(); // one or multiple RPC calls will be issued;
+      String contentString = Base64.getEncoder().encodeToString(content);
+      return new InputQuery(contentString, filePathStr);}
+
+      else{
       return new InputQuery(Files.readString(fileName), filePathStr);
+      }
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
