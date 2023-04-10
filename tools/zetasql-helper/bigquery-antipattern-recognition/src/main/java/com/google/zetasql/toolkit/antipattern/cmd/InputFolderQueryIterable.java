8e41308a945ebad45a18121c1d1eca3c5e22d0cd
Celia Ji <celiaji@google.com>
2023-04-10 02:06:40 +0000

    add gcs file and folder parsing

--- tools/zetasql-helper/bigquery-antipattern-recognition/src/main/java/com/google/zetasql/toolkit/antipattern/cmd/InputFolderQueryIterable.java
+++ tools/zetasql-helper/bigquery-antipattern-recognition/src/main/java/com/google/zetasql/toolkit/antipattern/cmd/InputFolderQueryIterable.java
@@ -5,6 +5,9 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Iterator;
 import java.util.List;
+import com.google.cloud.storage.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class InputFolderQueryIterable implements Iterator<InputQuery> {
 
@@ -24,7 +27,21 @@ public class InputFolderQueryIterable implements Iterator<InputQuery> {
     String filePathStr = filePathIterator.next();
     Path fileName = Path.of(filePathStr);
     try {
-      return new InputQuery(Files.readString(fileName), filePathStr);
+      if (filePathStr.startsWith("gs://")){
+        String trimFilePathStr = filePathStr.replace("gs://", "");
+        List<String> list = new ArrayList(Arrays.asList(trimFilePathStr.split("/")));
+        String bucket = list.get(0);
+        list.remove(0);
+        String filename = String.join("/", list);
+        Storage storage = StorageOptions.newBuilder()
+                .build()
+                .getService();
+        Blob blob = storage.get(bucket, filename);
+        String fileContent = new String(blob.getContent());
+        return new InputQuery(fileContent, filePathStr);}
+      else{
+        return new InputQuery(Files.readString(fileName), filePathStr);
+      }
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
